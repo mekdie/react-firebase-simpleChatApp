81839ba0aa217ff273d580275ddd6c49e988faeb
McDony Lee <mcdonyleee@gmail.com>
2023-01-21 23:43:37 +1100

    Added basic skeleton for chat app with firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+//firebase SDK
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectioData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+    //config
+    apiKey: "AIzaSyC_jTZES2d4nYaqpUCiOdos5g6OXTIKqvY",
+    authDomain: "react-basic-chat-5dccb.firebaseapp.com",
+    projectId: "react-basic-chat-5dccb",
+    storageBucket: "react-basic-chat-5dccb.appspot.com",
+    messagingSenderId: "173222891115",
+    appId: "1:173222891115:web:7e568d2354ba2ac57f2b2e",
+    measurementId: "G-M7MSN37PL1",
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <div className="App"></div>;
 }
 
 export default App;
